679638f43c7dc127a1fdbaba019aee237a22f984
Lahiru Jayamanna <lsampath999@gmail.com>
2019-06-12 01:10:42 +0800

    add axios support and youtube api configuration

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyDGDwWnF2O-GrOs5Ta71S2zEDMtVGe1SBQ';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 10,
+    key: API_KEY
+  }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-  onSearchTermSubmit = term => {
-    console.log(term);
+  state = { videos: [] };
+
+  onSearchTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSearchTermSubmit={this.onSearchTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
